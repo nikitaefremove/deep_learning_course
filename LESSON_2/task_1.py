25246db8e13288bc85b3a4efd69b555a00e82f28
Nikita <nikitaefremove@yandex.ru>
2023-08-22 23:47:58 +0700

    add delete task_2  Please enter the commit message for your changes. Lines starting

--- LESSON_2/task_1.py
+++ LESSON_2/task_1.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import Optimizer
 
 
 def create_model():
@@ -8,3 +10,19 @@ def create_model():
         nn.Linear(10, 1))
 
     return model
+
+
+def train(model: nn.Module, data_loader: DataLoader, optimizer: Optimizer, loss_fn):
+    model.train()
+    total_loss = 0
+
+    for i, (x, y) in enumerate(data_loader):
+        optimizer.zero_grad()
+        output = model(x)
+        loss = loss_fn(output, y)
+        total_loss += loss.item()
+        loss.backward()
+        print(f'{loss.item():.5f}')
+        optimizer.step()
+
+    return total_loss / len(data_loader)
