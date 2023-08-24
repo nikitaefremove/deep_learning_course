ff36524ffadbc056c59f5bb65898b929edb386f1
Nikita <nikitaefremove@yandex.ru>
2023-08-24 13:02:35 +0700

    add functions to Lesson_3

--- LESSON_3/tasks.py
+++ LESSON_3/tasks.py
@@ -1,18 +1,28 @@
+import numpy as np
 import torch
 import torch.nn as nn
+import torchvision.transforms as T
+from PIL import Image
+from matplotlib import cm
+from time import perf_counter
 from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
 from torch.optim import Optimizer
 
 
-def create_model():
-    model = nn.Sequential(
-        nn.Linear(100, 10),
-        nn.ReLU(),
-        nn.Linear(10, 1))
+# Get count of parameters of convolutional Neural Network
+def count_parameters_conv(in_channels: int,
+                          out_channels: int,
+                          kernel_size: int,
+                          bias: bool):
+    if bias == True:
+        return (in_channels * kernel_size ** 2 + 1) * out_channels
 
-    return model
+    else:
+        return (in_channels * kernel_size ** 2) * out_channels
 
 
+# Train model. Return average of loss
 def train(model: nn.Module, data_loader: DataLoader, optimizer: Optimizer, loss_fn):
     model.train()
     total_loss = 0
@@ -29,6 +39,7 @@ def train(model: nn.Module, data_loader: DataLoader, optimizer: Optimizer, loss_
     return total_loss / len(data_loader)
 
 
+# Evaluate function. Return average of loss
 @torch.inference_mode()
 def evaluate(model: nn.Module, data_loader: DataLoader, loss_fn):
     model.eval()
@@ -42,12 +53,46 @@ def evaluate(model: nn.Module, data_loader: DataLoader, loss_fn):
     return total_loss / len(data_loader)
 
 
-def count_parameters_conv(in_channels: int,
-                          out_channels: int,
-                          kernel_size: int,
-                          bias: bool):
-    if bias == True:
-        return (in_channels * kernel_size ** 2 + 1) * out_channels
+# Load MNIST Dataset. Split by train and validation parts.
+mnist_train = MNIST(
+    "../datasets/mnist",
+    train=True,
+    download=True,
+    transform=T.ToTensor()
+)
 
-    else:
-        return (in_channels * kernel_size ** 2) * out_channels
+mnist_valid = MNIST(
+    "../datasets/mnist",
+    train=False,
+    download=True,
+    transform=T.ToTensor()
+)
+
+# Load MNIST datasets with DataLoader.
+# Add batches, and shuffle data for better result
+
+train_loader = DataLoader(mnist_train, batch_size=64, shuffle=True)
+valid_loader = DataLoader(mnist_valid, batch_size=64, shuffle=False)
+
+
+# Create model function
+def create_mlp_model():
+    model = nn.Sequential(
+        nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),
+
+        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=5),
+        nn.ReLU(),
+        nn.MaxPool2d(kernel_size=2),
+
+        nn.Flatten(),
+        nn.Linear(4 * 4 * 64, 256),
+        nn.ReLU(),
+        nn.Linear(256, 10)
+    )
+
+    return model
+
+
+model = create_mlp_model()
