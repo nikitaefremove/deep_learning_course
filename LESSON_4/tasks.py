81d060da777b54e6d21ca5451b14a72b31397812
Nikita <nikitaefremove@yandex.ru>
2023-08-25 20:10:44 +0700

    add get_augmantations

--- LESSON_4/tasks.py
+++ LESSON_4/tasks.py
@@ -1,12 +1,36 @@
 import torch
+import torchvision.transforms as T
 
 
 def get_normalize(features: torch.Tensor):
-    # размер [N, C, H, W]
-    # N это количество объектов, C — количество каналов, H, W — размеры изображений
-    # будет возвращать поканальное среднее и поканальное стандартное отклонение
+    # size [N, C, H, W]
+    # N is amount of objects, C — amount of chanel, H, W — size of image
+    # return mean by chanel and standard deviation by chanel
 
     mean = features.mean(dim=(0, 2, 3))
     std = features.std(dim=(0, 2, 3))
 
     return mean, std
+
+
+def get_augmentations(train: bool = True) -> T.Compose:
+    means = (0.49139968, 0.48215841, 0.44653091)
+    stds = (0.24703223, 0.24348513, 0.26158784)
+    if train:
+        return T.Compose([
+            T.Resize((224, 224)),
+            T.CenterCrop(10),
+            T.Grayscale(),
+            T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            T.RandAugment(),
+            T.ToTensor(),
+            T.Normalize(mean=means, std=stds)
+        ]
+        )
+    else:
+        return T.Compose([
+            T.Resize((224, 224)),
+            T.ToTensor(),
+            T.Normalize(mean=means, std=stds)
+        ]
+        )
