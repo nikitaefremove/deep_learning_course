be3b8dc740cca387b840ae38ef0800a8ed3eed68
Nikita <nikitaefremove@yandex.ru>
2023-08-25 21:04:57 +0700

    add predict func

--- LESSON_4/tasks.py
+++ LESSON_4/tasks.py
@@ -1,7 +1,10 @@
 import torch
+import torch.nn as nn
 import torchvision.transforms as T
+from torch.utils.data import DataLoader
 
 
+# Normalization function. Return mean and standard deviation
 def get_normalize(features: torch.Tensor):
     # size [N, C, H, W]
     # N is amount of objects, C — amount of chanel, H, W — size of image
@@ -13,6 +16,9 @@ def get_normalize(features: torch.Tensor):
     return mean, std
 
 
+# Function for augmentation.
+# Resize image,  make augmentation for train (crop, b&w, blur, random)
+# Make tensor from train and test and normalize
 def get_augmentations(train: bool = True) -> T.Compose:
     means = (0.49139968, 0.48215841, 0.44653091)
     stds = (0.24703223, 0.24348513, 0.26158784)
@@ -34,3 +40,18 @@ def get_augmentations(train: bool = True) -> T.Compose:
             T.Normalize(mean=means, std=stds)
         ]
         )
+
+
+def predict(model: nn.Module, loader: DataLoader, device: torch.device):
+    predictions = []
+    model = model.to(device)
+    model.eval()  # set the model to evaluation mode
+
+    with torch.no_grad():  # deactivate autograd
+        for x, _ in loader:  # we do not need 'y' for predictions
+            x = x.to(device)
+            output = model(x)  # forward pass
+            preds = output.argmax(dim=1)  # get the predicted classes
+            predictions.append(preds)
+
+    return torch.cat(predictions)
