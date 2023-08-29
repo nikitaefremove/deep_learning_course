32d33950e9c72cf3493b9ade0359c0d49c195674
Nikita <nikitaefremove@yandex.ru>
2023-08-29 13:25:34 +0700

    add skip_con sunc

--- LESSON_5/advanced_skip_connection_conv_cifar.py
+++ LESSON_5/advanced_skip_connection_conv_cifar.py
@@ -1,47 +1,53 @@
 import torch.nn as nn
 
 
-def advanced_conv_cifar():
-    # we have 32 * 32 * 3 after resizing
-    return nn.Sequential(
-        nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),  # 32 x 32 x 32
-        nn.ReLU(),
-        nn.BatchNorm2d(32),
+class create_simple_conv_cifar(nn.Module):
+    def __init__(self):
+        super(create_simple_conv_cifar, self).__init__()
 
-        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),  # 32 x 32 x 32
-        nn.ReLU(),
-        nn.BatchNorm2d(32),
+        # we have 32 * 32 * 3
 
-        nn.MaxPool2d(2),  # 16 x 16 x 32
-        nn.Dropout2d(p=0.25),
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),  # 32 x 32 x 32
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
 
-        nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),  # 16 x 16 x 64
-        nn.ReLU(),
-        nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),  # 32 x 32 x 32
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
 
-        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),  # 16 x 16 x 64
-        nn.ReLU(),
-        nn.BatchNorm2d(64),
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),  # 16 x 16 x 64
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
 
-        nn.MaxPool2d(2),  # 8 x 8 x 64
-        nn.Dropout2d(p=0.25),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),  # 16 x 16 x 64
+            nn.BatchNorm2d(64),
+            nn.ReLU()
+        )
 
-        nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),  # 8 x 8 x 128
-        nn.BatchNorm2d(128),
-        nn.ReLU(),
+        self.block_3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),  # 8 x 8 x 128
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
 
-        nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),  # 8 x 8 x 128
-        nn.BatchNorm2d(128),
-        nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),  # 8 x 8 x 128
+            nn.BatchNorm2d(128),
+            nn.ReLU()
+        )
 
-        nn.MaxPool2d(2),  # 4 x 4 x 128
-        nn.Dropout2d(p=0.25),
+        self.maxpool = nn.MaxPool2d(2)
 
-        nn.Flatten(),
+        self.drop = nn.Dropout2d(p=0.25)
 
-        nn.Linear(4 * 4 * 128, 512),
-        nn.BatchNorm1d(512),
-        nn.Dropout(p=0.25),
-        nn.ReLU(),
-        nn.Linear(512, 10)
-    )
+        self.flat = nn.Flatten()
+
+        self.relu = nn.ReLU()
+
+        self.fc1 = nn.Linear(4 * 4 * 128, 512)
+
+        self.fc2 = nn.Linear(512, 10)
+
+    def forward(self, x):
+        return self.net(x)
