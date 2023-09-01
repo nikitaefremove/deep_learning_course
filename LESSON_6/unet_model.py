109d39905e66eb036735cebb8848fc08873276ac
Nikita <nikitaefremove@yandex.ru>
2023-09-02 00:56:14 +0700

    have to fix model

--- LESSON_6/unet_model.py
+++ LESSON_6/unet_model.py
@@ -1,5 +1,4 @@
-import torch
-import torch.nn as nn
+#!g1.1
 
 
 def conv_plus_conv(in_channels: int, out_channels: int):
@@ -35,15 +34,17 @@ class UNET(nn.Module):
     def __init__(self):
         super().__init__()
 
-        base_channels = 16
+        base_channels = 32
 
         self.down1 = conv_plus_conv(3, base_channels)
         self.down2 = conv_plus_conv(base_channels, base_channels * 2)
+        self.down3 = conv_plus_conv(base_channels * 2, base_channels * 4)
 
         self.up1 = conv_plus_conv(base_channels * 2, base_channels)
         self.up2 = conv_plus_conv(base_channels * 4, base_channels)
+        self.up3 = conv_plus_conv(base_channels * 3, base_channels)
 
-        self.bottleneck = conv_plus_conv(base_channels * 2, base_channels * 2)
+        self.bottleneck = conv_plus_conv(base_channels * 4, base_channels * 4)
 
         self.out = nn.Conv2d(in_channels=base_channels, out_channels=3, kernel_size=1)
 
@@ -52,31 +53,31 @@ class UNET(nn.Module):
     def forward(self, x):
         # x.shape = (N, N, 3)
 
-        residual1 = self.down1(x)  # N, N, 3 -> N, N, base_channels
-        x = self.downsample(residual1)  # N, N, base_channels -> N // 2, N // 2, base_channels
+        residual1 = self.down1(x)  # N, N, base_channels
+        x = self.downsample(residual1)  # N // 2, N // 2, base_channels
 
-        residual2 = self.down2(x)  # N // 2, N // 2, base_channels -> N // 2, N // 2, base_channels * 2
-        x = self.downsample(
-            residual2)  # N // 2, N // 2, base_channels * 2 -> N // 4, N // 4, base_channels * 2
+        residual2 = self.down2(x)  # N // 2, N // 2, base_channels * 2
+        x = self.downsample(residual2)  # N // 4, N // 4, base_channels * 2
 
-        # LATENT SPACE DIMENSION DIM = N // 4
-        # SOME MANIPULATION MAYBE
-        x = self.bottleneck(x)  # N // 4, N // 4, base_channels * 2 -> N // 4, N // 4, base_channels * 2
-        # SOME MANIPULATION MAYBE
-        # LATENT SPACE DIMENSION DIM = N // 4
+        residual3 = self.down3(x)  # N // 4, N // 4, base_channels * 4
+        x = self.downsample(residual3)  # N // 8, N // 8, base_channels * 4
 
-        x = nn.functional.interpolate(x,
-                                      scale_factor=2)  # N // 4, N // 4, base_channels * 2 -> N // 2, N // 2, base_channels * 2
-        x = torch.cat((x, residual2),
-                      dim=1)  # N // 2, N // 2, base_channels * 2 -> N // 2, N // 2, base_channels * 4
-        x = self.up2(x)  # N // 2, N // 2, base_channels * 4 -> N // 2, N // 2, base_channels
+        # LATENT SPACE DIMENSION DIM = N // 8
+        x = self.bottleneck(x)  # N // 8, N // 8, base_channels * 4
+        # LATENT SPACE DIMENSION DIM = N // 8
 
-        x = nn.functional.interpolate(x,
-                                      scale_factor=2)  # x.shape: (N // 2, N // 2, base_channels) -> (N, N, base_channels)
-        x = torch.cat((x, residual1),
-                      dim=1)  # N, N, base_channels -> N, N, base_channels * 2
-        x = self.up1(x)  # N, N, base_channels * 2 -> N, N, base_channels
+        x = nn.functional.interpolate(x, scale_factor=2)  # N // 4, N // 4, base_channels * 4
+        x = torch.cat((x, residual3), dim=1)  # N // 4, N // 4, base_channels * 8
+        x = self.up3(x)  # N // 4, N // 4, base_channels
 
-        x = self.out(x)  # N, N, base_channels -> N, N, 3
+        x = nn.functional.interpolate(x, scale_factor=2)  # N // 2, N // 2, base_channels
+        x = torch.cat((x, residual2), dim=1)  # N // 2, N // 2, base_channels * 4
+        x = self.up2(x)  # N // 2, N // 2, base_channels
+
+        x = nn.functional.interpolate(x, scale_factor=2)  # N, N, base_channels
+        x = torch.cat((x, residual1), dim=1)  # N, N, base_channels * 2
+        x = self.up1(x)  # N, N, base_channels
+
+        x = self.out(x)  # N, N, 3  # Change here
 
         return x
