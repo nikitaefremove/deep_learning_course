545ca09f55afcb5464035da0a1011e7ca940392e
Nikita <nikitaefremove@yandex.ru>
2023-09-04 18:57:51 +0700

    add update

--- LESSON_6/unet_model.py
+++ LESSON_6/unet_model.py
@@ -1,13 +1,9 @@
-#!g1.1
+import torch
+import torch.nn as nn
 
 
-def conv_plus_conv(in_channels: int, out_channels: int):
-    """
-    Makes UNet block
-    :param in_channels: input channels
-    :param out_channels: output channels
-    :return: UNet block
-    """
+def conv_plus_conv(in_channels: int,
+                   out_channels: int):
     return nn.Sequential(
         nn.Conv2d(
             in_channels=in_channels,
@@ -39,12 +35,16 @@ class UNET(nn.Module):
         self.down1 = conv_plus_conv(3, base_channels)
         self.down2 = conv_plus_conv(base_channels, base_channels * 2)
         self.down3 = conv_plus_conv(base_channels * 2, base_channels * 4)
+        self.down4 = conv_plus_conv(base_channels * 4, base_channels * 8)
+        self.down5 = conv_plus_conv(base_channels * 8, base_channels * 16)
 
         self.up1 = conv_plus_conv(base_channels * 2, base_channels)
         self.up2 = conv_plus_conv(base_channels * 4, base_channels)
-        self.up3 = conv_plus_conv(base_channels * 3, base_channels)
+        self.up3 = conv_plus_conv(base_channels * 8, base_channels * 2)
+        self.up4 = conv_plus_conv(base_channels * 16, base_channels * 4)
+        self.up5 = conv_plus_conv(base_channels * 32, base_channels * 8)
 
-        self.bottleneck = conv_plus_conv(base_channels * 4, base_channels * 4)
+        self.bottleneck = conv_plus_conv(base_channels * 16, base_channels * 16)
 
         self.out = nn.Conv2d(in_channels=base_channels, out_channels=3, kernel_size=1)
 
@@ -52,32 +52,43 @@ class UNET(nn.Module):
 
     def forward(self, x):
         # x.shape = (N, N, 3)
+        residual1 = self.down1(x)
+        x = self.downsample(residual1)
 
-        residual1 = self.down1(x)  # N, N, base_channels
-        x = self.downsample(residual1)  # N // 2, N // 2, base_channels
+        residual2 = self.down2(x)
+        x = self.downsample(residual2)
 
-        residual2 = self.down2(x)  # N // 2, N // 2, base_channels * 2
-        x = self.downsample(residual2)  # N // 4, N // 4, base_channels * 2
+        residual3 = self.down3(x)
+        x = self.downsample(residual3)
 
-        residual3 = self.down3(x)  # N // 4, N // 4, base_channels * 4
-        x = self.downsample(residual3)  # N // 8, N // 8, base_channels * 4
+        residual4 = self.down4(x)
+        x = self.downsample(residual4)
 
-        # LATENT SPACE DIMENSION DIM = N // 8
-        x = self.bottleneck(x)  # N // 8, N // 8, base_channels * 4
-        # LATENT SPACE DIMENSION DIM = N // 8
+        residual5 = self.down5(x)
+        x = self.downsample(residual5)
 
-        x = nn.functional.interpolate(x, scale_factor=2)  # N // 4, N // 4, base_channels * 4
-        x = torch.cat((x, residual3), dim=1)  # N // 4, N // 4, base_channels * 8
-        x = self.up3(x)  # N // 4, N // 4, base_channels
+        x = self.bottleneck(x)
 
-        x = nn.functional.interpolate(x, scale_factor=2)  # N // 2, N // 2, base_channels
-        x = torch.cat((x, residual2), dim=1)  # N // 2, N // 2, base_channels * 4
-        x = self.up2(x)  # N // 2, N // 2, base_channels
+        x = nn.functional.interpolate(x, scale_factor=2)
+        x = torch.cat((x, residual5), dim=1)
+        x = self.up5(x)
 
-        x = nn.functional.interpolate(x, scale_factor=2)  # N, N, base_channels
-        x = torch.cat((x, residual1), dim=1)  # N, N, base_channels * 2
-        x = self.up1(x)  # N, N, base_channels
+        x = nn.functional.interpolate(x, scale_factor=2)
+        x = torch.cat((x, residual4), dim=1)
+        x = self.up4(x)
 
-        x = self.out(x)  # N, N, 3  # Change here
+        x = nn.functional.interpolate(x, scale_factor=2)
+        x = torch.cat((x, residual3), dim=1)
+        x = self.up3(x)
+
+        x = nn.functional.interpolate(x, scale_factor=2)
+        x = torch.cat((x, residual2), dim=1)
+        x = self.up2(x)
+
+        x = nn.functional.interpolate(x, scale_factor=2)
+        x = torch.cat((x, residual1), dim=1)
+        x = self.up1(x)
+
+        x = self.out(x)
 
         return x
